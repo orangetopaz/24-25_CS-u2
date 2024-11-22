717efc6e3dbc119fbb3735b1370bb3f6bfe7efcc
winter-sand <09husky.13@gmail.com>
2024-11-22 18:44:57 +0300

    finished up with some I2C functoins in kernel, connecting to my PCF8574 LCD screen. doing some functions for specificaly commands and data sending to the lcd

--- kernel.c
+++ kernel.c
@@ -3,12 +3,12 @@
 
 // The GPIO registers base address.
 
-#define I2C_BASE 0x3F804000  // Adjust based on Pi version
-#define I2C_C    *(volatile uint32_t *)(I2C_BASE + 0x00)  // all different regesters for reading and writing different things.
-#define I2C_S    *(volatile uint32_t *)(I2C_BASE + 0x04)  // C= Control Regester
-#define I2C_DLEN *(volatile uint32_t *)(I2C_BASE + 0x08)  // S= Status Regester
-#define I2C_A    *(volatile uint32_t *)(I2C_BASE + 0x0C)  // DLEN= Control Regester
-#define I2C_FIFO *(volatile uint32_t *)(I2C_BASE + 0x10)  // FIFO= Control Regester
+#define I2C_BASE 0x3F804000  // Adjust based on Pi version  // all different regesters for reading and writing different things.
+#define I2C_C    *(volatile uint32_t *)(I2C_BASE + 0x00)  // C= Control Regester
+#define I2C_S    *(volatile uint32_t *)(I2C_BASE + 0x04)  // S= Status Regester
+#define I2C_DLEN *(volatile uint32_t *)(I2C_BASE + 0x08)  // DLEN= Data Length
+#define I2C_A    *(volatile uint32_t *)(I2C_BASE + 0x0C)  // A= Slave regester, the 1st 7 bits of which contain the address of the I2C component (unique to each device, it depends.) for LCD displays, usualy 0x27, but can be 0x3F rarely
+#define I2C_FIFO *(volatile uint32_t *)(I2C_BASE + 0x10)  // FIFO= first in first out, very small amount of storage
 
 /* enum {  // Probobly not going to use this as I can't find a ttc to usb, but good just in case
 
@@ -38,28 +38,23 @@
   UART0_TDR    = (UART0_BASE + 0x8C),
 }; */
 
-#define GPIO_BASE      0x3f200000  // for raspi2 & 3, 0x20200000 for raspi1
-volatile uint32_t *gpio_base = (volatile uint32_t *)GPIO_BASE;  // turn gpio base into an address (pointer) to be writen to with a new versoin of the variable that is now lowercase
+#define GPIO_BASE      0x3F200000  // for raspi2 & 3, 0x20200000 for raspi1
+  // turn gpio base into an address (pointer) to be writen to with a new versoin of the variable that is now lowercase
 #define GPFSEL0        0x00
 #define GPFSEL1        0x04
 #define GPFSEL2        0x08
 #define GPFSEL3        0x0c
 #define GPFSEL4        0x10
 #define GPFSEL5        0x14
-#define GPFSET0        0x1c
-volatile uint32_t *gpfset0 = (volatile uint32_t *)(GPIO_BASE + GPFSET0);
-#define GPFCLR0        0x28
-volatile uint32_t *gpfclr0 = (volatile uint32_t *)(GPIO_BASE + GPFCLR0);
-#define GPFSET1        0x20
-volatile uint32_t *gpfset1 = (volatile uint32_t *)(GPIO_BASE + GPFSET1);
-#define GPFCLR1        0x2c
-volatile uint32_t *gpfclr1 = (volatile uint32_t *)(GPIO_BASE + GPFCLR1);
-#define GPIOVAL        0x200000
+#define GPFSET0        *(volatile uint32_t *)(GPIO_BASE + 0x1c)
+#define GPFCLR0        *(volatile uint32_t *)(GPIO_BASE + 0x28)
+#define GPFSET1        *(volatile uint32_t *)(GPIO_BASE + 0x20)
+#define GPFCLR1        *(volatile uint32_t *)(GPIO_BASE + 0x2c)
 
 void pinFunc(unsigned int pinN, uint32_t funcSet){
   // function to bits:
-    // setToOutput: 0b000
-    // setToIutput: 0b001
+    // setToInput: 0b000
+    // setToOutput: 0b001
     // setToAltFuc0: 0b100
     // setToAltFuc1: 0b101
     // setToAltFuc2: 0b110
@@ -74,10 +69,10 @@ void pinFunc(unsigned int pinN, uint32_t funcSet){
     // pins 40-49: GPFSEL4
     // pins 50-53: GPFSEL5
   unsigned int bitPos = 3*(pinN%10);
-  volatile uint32_t *gpfsel = gpio_base + (pinN / 10)*4;  // intager division
+  volatile uint32_t* GPFSEL = (volatile uint32_t *)(GPIO_BASE + ((pinN / 10) * 4));  // intager division
   // declare a pointer with a * before the name at any point, refrence the target of the pointer and not just the pos by including the defferance operator, which is conincedentaly also an astrix before a pointer var
-  *gpfsel &= ~(0b111 << bitPos);  // clears the position of the funcset area, by putting ...000000011100000000... at the pos, then inverting to only and (clear) the necessary 3 bits, and set everything else to 1, so that it preserves the original setting when anded.
-  *gpfsel |= (funcSet << bitPos);  // gets the full 32 bit func like ...00000000"001"000000... then oring it to make sure not to clear anytning else by setting it to 0
+  *GPFSEL &= ~(0b111 << bitPos);  // clears the position of the funcset area, by putting ...000000011100000000... at the pos, then inverting to only and (clear) the necessary 3 bits, and set everything else to 1, so that it preserves the original setting when anded.
+  *GPFSEL |= (funcSet << bitPos);  // gets the full 32 bit func like ...00000000"001"000000... then oring it to make sure not to clear anytning else by setting it to 0
 
 }
 
@@ -86,33 +81,62 @@ void pinFunc(unsigned int pinN, uint32_t funcSet){
   // pins 32-53: CLR1/SET1
 void pinOn(unsigned int pinN){  // 1 leftshifted by the pin number into the regester
   if (pinN <= 31){
-    *gpfset0 = (1 << pinN);
+    GPFSET0 = (1 << pinN);  // assignes to the regester its pointing at. deference operator * not neccessary as it's baked into the definition at the top
   }
   else if (pinN >= 32){
-    *gpfset1 = (1 << (pinN-32));
+    GPFSET1 = (1 << (pinN-32));
   }
 }
 void pinOff(unsigned int pinN){
   if (pinN <= 31){
-    *gpfclr0 = (1 << pinN);
+    GPFCLR0 = (1 << pinN);
   }
   else if (pinN >= 32){
-    *gpfclr1 = (1 << (pinN-32));
+    GPFCLR1 = (1 << (pinN-32));
   }
 }
 
 void OSDelay(int reps){
   while (reps--) {
-    // empty loop to create delay, compiler might optomize but I don't trust macos that much
+    asm volatile("nop"); // empty loop to create delay, compiler might optomize but I don't trust macos that much
   }
 }
 
-int kernel_main(){
-  pinFunc(21, 0b000);
-  pinOn(21);  // should turn the rgb led on the bread board blue
-  pinOn(47);  // should turn on the act led
-  OSDelay(1000);
-  pinOff(21);  // should turn the rgb led off
-  pinOff(47);  // should turn off the act led
-  return 0;  // branches back to the halt function in the boot.S, don't include if boot.S doen't have halt, do a while(1) loop instead.
+void I2CInitalize() {
+  // Set the I2C clock rate, address, and enable I2C mode
+  I2C_C = 0;  // Disable I2C temporarily for configuration
+  I2C_A = 0x27;  // Address for your LCD, adjust as needed. only the 1st 7 bits of the 32 of this regester matter/are written to, it is the address. in this case, it's address 39, or 0b100111, or 0x27
+  // Set clock rate by configuring I2C_C (Clock Control)
+  // Enable the interface after configuration
+  I2C_C |= (1 << 15);  // Enable I2C. Bit 15 is the BSC (Broadcom (manufacturer of raspi2 processor) Serial Controller) enable/disable (1/0) (page 29)
+}
+
+void I2CByteSend(uint8_t cmdOrData){
+  I2C_DLEN = 1;  // DLEN has 16 bits to store the length. it counts through this to send the bits one at a time
+  I2C_FIFO = cmdOrData;  // there are some commands that I can send to the screen to do certain things. EG, 0x01 clears, 0x0C turns on w/o cursor, 0x06 set auto increment for cursor, 0x30 wake up (send multiple times), 0x28 turn on 4 bit mode and 2 line display 
+  I2C_C |= (1 << 7);  // sets the start (7) bit to 1, to start transmission of command
+  while (!(I2C_S & (1 << 1)));  // checks if the DONE (1) bit, anded with 1 = 1. if it equals 0, then invertend and the loop continues, but if 1, inverted is 0, loop ends
+  I2C_S |= (1 << 1); // clear the DONE bit 
+}
+
+/*
+the FIFO regester sends data to the LCD. the bits work out like this:
+bits 0:3 - Char/Command
+bit 4 - Regester select (RS) (0 for sending command, 1 for sending char/data)
+bit 5 - read/write (R/W) (0 for writing to LCD, 1 for reading from LCD)
+bit 6 - enable (E), rising edge signal (0 -> 1), then falling edge (1 -> 0) (send command twice with same data, exept change enable from 1 to 0) latches then sends data to LCD
+bit 7 - backlight control (BL) (if appliccable). usualy 1 for backlight on, 0 for backlight off
+*/
+unsigned short backlight = 1;  // bool backlight = true;
+void LCDSendCmd(uint8_t cmd){  
+  uint8_t settings = 0b00000000;  // & 0b[BL]0[RS = 0][R/W = 0]0000, *sends* a *command*
+  settings |= (backlight << 7);  //0b[backlight]0000000
+  
+}
+void LCDSendChar(char ch){  
+  
+}
+
+void kernel_main() {
+  I2CInitalize();
 }
